52d5d6da8f829b409170d613804ddaa4a1509d59
MinhCuong0202 <153370105+MinhCuong0202@users.noreply.github.com>
2023-12-14 16:20:29 +0700

    Add files via upload

---
+++ Btvn1.cpp
@@ -0,0 +1,17 @@
+Compiling single file...
+--------
+- Filename: D:\ht7.cpp
+- Compiler Name: TDM-GCC 4.9.2 64-bit Release
+
+Processing C++ source file...
+--------
+- C++ Compiler: C:\Program Files (x86)\Dev-Cpp\MinGW64\bin\g++.exe
+- Command: g++.exe "D:\ht7.cpp" -o "D:\ht7.exe"  -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\include" -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\include" -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include" -I"C:\Program Files (x86)\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include\c++" -L"C:\Program Files (x86)\Dev-Cpp\MinGW64\lib" -L"C:\Program Files (x86)\Dev-Cpp\MinGW64\x86_64-w64-mingw32\lib" -static-libgcc
+
+Compilation results...
+--------
+- Errors: 0
+- Warnings: 0
+- Output Filename: D:\ht7.exe
+- Output Size: 133,3701171875 KiB
+- Compilation Time: 0,70s
